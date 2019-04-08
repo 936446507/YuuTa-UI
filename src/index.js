d865ce598a16b644bddee6630ab8c1d2db864bf3
arima <936446507@qq.com>
2019-04-08 23:34:06 +0800

    add swiper component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import YTButton from './packages/button'
 import YTButtonGroup from './packages/button-group'
 import YTRadio from './packages/radio'
+import YTSwiper from './packages/swiper'
+import YTSwiperItem from './packages/swiper-item'
 
 import './assets/font/iconfont.css';
 
 const components = [
-  YTRadio,
   YTButton,
-  YTButtonGroup
+  YTButtonGroup,
+  YTRadio,
+  YTSwiper,
+  YTSwiperItem
 ]
 
 const install = function(Vue, opts = {}) {
